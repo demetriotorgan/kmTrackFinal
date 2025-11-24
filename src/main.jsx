720d52d4854f302a5a5332397d2bd49d0a9e41e0
demetriotorgan <demetriotorgan@hotmail.com>
2025-11-24 12:40:47 -0300

    erroArquivo

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { configurarOfflineInterceptor } from './services/offLineInterceptor.js'
+import { configurarOfflineInterceptor } from './services/offlineInterceptor.js'
 import {iniciarMonitoramento} from './services/syncManager.js'
 
 // ativa interceptador
